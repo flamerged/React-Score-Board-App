6968cb5010a47981cda43002e67039229aa4d3ad
flamerged <mersad.ajanovic@googlemail.com>
2019-10-12 21:07:06 +0200

    added player and counter components

--- app.js
+++ app.js
@@ -1,13 +1,41 @@
-function Header() {
-	return (
-		<header>
-			<h1>Scoreboard</h1>
-			<span>Players: 1</span>
-		</header>
-	);
-}
+const Header = () => {
+    return (
+        <header>
+            <h1>Scoreboard</h1>
+            <span>Players: 1</span>
+        </header>
+    );
+};
 
-ReactDOM.render(
-	header,
-	document.getElementById("root")
-);
+const Player = () => {
+    return (
+        <div className="player">
+            <span className="player-name">Guil</span>
+            <Counter />
+        </div>
+    );
+};
+
+const Counter = () => {
+    return (
+        <div className="counter">
+            <button className="counter-action decrement"> - </button>
+            <span className="counter-score">35</span>
+            <button className="counter-action increment"> + </button>
+        </div>
+    );
+};
+
+const App = () => {
+    return (
+        <div className="scoreboard">
+            <Header />
+
+            {/* Player list*/}
+
+            <Player />
+        </div>
+    );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
