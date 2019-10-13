7ae194e6cdbc84a166be6017dce460e8cb09a2f2
flamerged <mersad.ajanovic@googlemail.com>
2019-10-13 15:49:00 +0200

    added props to some components to make them reusable

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
-const Header = () => {
+const Header = props => {
     return (
         <header>
-            <h1>Scoreboard</h1>
-            <span>Players: 1</span>
+            <h1>{props.title}</h1>
+            <span>Players: {props.totalPlayers}</span>
         </header>
     );
 };
 
-const Player = () => {
+const Player = props => {
     return (
         <div className="player">
-            <span className="player-name">Guil</span>
-            <Counter />
+            <span className="player-name">{props.playerName}</span>
+            <Counter score={props.playerScore} />
         </div>
     );
 };
 
-const Counter = () => {
+const Counter = props => {
     return (
         <div className="counter">
             <button className="counter-action decrement"> - </button>
-            <span className="counter-score">35</span>
+            <span className="counter-score">{props.score}</span>
             <button className="counter-action increment"> + </button>
         </div>
     );
@@ -29,11 +29,11 @@ const Counter = () => {
 const App = () => {
     return (
         <div className="scoreboard">
-            <Header />
+            <Header title="Scoreboard" totalPlayers={1} />
 
             {/* Player list*/}
 
-            <Player />
+            <Player playerName="Guil" playerScore={35} />
         </div>
     );
 };
